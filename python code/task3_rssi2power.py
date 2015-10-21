f4e5c2c55f4e0f7f960bade253a61b75d273bef2
tenglongroy <379600075@qq.com>
2015-10-21 16:53:02 +1100

    rssi

--- python code/task3_rssi2power.py
+++ python code/task3_rssi2power.py
@@ -1,3 +1,10 @@
-line = input("input the RSSI(dBm) collected\n")
-result = line.rstrip().split()
-print(result)
+import math
+while(True):
+    line = input("input the RSSI(dBm) collected\n")
+    dbm = line.rstrip().split()
+    #print(result)
+    #dBm = 10log(mW)
+    power = []
+    for item in dbm:
+        power.append(pow(10, int(item)/10))
+    print(power)
